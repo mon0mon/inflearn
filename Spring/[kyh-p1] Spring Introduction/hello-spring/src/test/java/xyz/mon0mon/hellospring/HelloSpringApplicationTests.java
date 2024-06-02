5b3cf161cbf70b32f92f1038f7e27a8eb354544c
이민기 <mon0mon@naver.com>
2024-06-02 10:50:06 +0900

    📝 study - Sec01 : View 환경설정

---
+++ Spring/[kyh-p1] Spring Introduction/hello-spring/src/test/java/xyz/mon0mon/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.mon0mon.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
