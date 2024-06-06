69fd392034ada4017a562653a0bf784a05ad4389
이민기 <mon0mon@naver.com>
2024-06-06 15:52:11 +0900

    📝 study - Sec02 : 스프링 웹 개발 기초 - Sec03 : 회원 관리 예제 - 백엔드 개발   - P02 : 회원 도메인과 리포지토리 만들기

---
+++ Spring/[kyh-p1] Spring Introduction/hello-spring/src/main/java/xyz/mon0mon/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package xyz.mon0mon.hellospring.repository;
+
+import xyz.mon0mon.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>();
+    private static Long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(sequence, member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values()
+                    .stream()
+                    .filter(member -> member.getName()
+                                            .equals(name))
+                    .findFirst();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
