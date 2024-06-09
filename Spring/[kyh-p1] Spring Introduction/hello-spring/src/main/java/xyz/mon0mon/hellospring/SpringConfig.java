d72dea31ee43702965ba273658ceb7364a945e8d
이민기 <mon0mon@naver.com>
2024-06-09 17:57:42 +0900

    📝 study - Sec06 스프링 DB 접근 기술   - Pt02 순수 JDBC

--- Spring/[kyh-p1] Spring Introduction/hello-spring/src/main/java/xyz/mon0mon/hellospring/SpringConfig.java
+++ Spring/[kyh-p1] Spring Introduction/hello-spring/src/main/java/xyz/mon0mon/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package xyz.mon0mon.hellospring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import xyz.mon0mon.hellospring.repository.JdbcMemberRepository;
 import xyz.mon0mon.hellospring.repository.MemberRepository;
 import xyz.mon0mon.hellospring.repository.MemoryMemberRepository;
 import xyz.mon0mon.hellospring.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
