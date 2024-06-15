882439dc1453138b39a2855fb69be23de7127c20
이민기 <mon0mon@naver.com>
2024-06-16 01:02:09 +0900

    📝 study - Track2 스프링 핵심 원리 - 기본편     - Sec04 스프링 컨테이너와 스프링 빈         - Pt07 다양한 설정 형식 지원 - 자바 코드, XML

---
+++ Spring/[kyh-p2] Spring Basic (OOP, Spring Contrainer, Spring Bean)/core/src/test/java/xyz/mon0mon/core/xml/XmlAppContext.java
@@ -0,0 +1,16 @@
+package xyz.mon0mon.core.xml;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import xyz.mon0mon.core.member.MemberService;
+
+public class XmlAppContext {
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("AppConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
