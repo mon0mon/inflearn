003be09db0a84dc981e987e65c32a0d2ca181a74
이민기 <mon0mon@naver.com>
2024-06-16 01:45:09 +0900

    📝 study - Track2 스프링 핵심 원리 - 기본편     - Sec04 스프링 컨테이너와 스프링 빈         - Pt08 스프링 빈 설정 메타 정보 - BeanDefinition

---
+++ Spring/[kyh-p2] Spring Basic (OOP, Spring Contrainer, Spring Bean)/core/src/test/java/xyz/mon0mon/core/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,39 @@
+package xyz.mon0mon.core.beandefinition;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import xyz.mon0mon.core.AppConfig;
+
+public class BeanDefinitionTest {
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplicationBean() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+    @Test
+    @DisplayName("빈 설정 메타정보 확인 - xml")
+    void findApplicationBeanXml() {
+        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("AppConfig.xml");
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+
+}
