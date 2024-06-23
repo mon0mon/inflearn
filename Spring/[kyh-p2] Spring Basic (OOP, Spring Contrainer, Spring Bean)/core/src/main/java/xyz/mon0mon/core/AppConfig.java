9bb560462e3656d9fcfc4fd6b48c6306f8e46246
이민기 <mon0mon@naver.com>
2024-06-23 21:42:02 +0900

    📝 study - Track2 스프링 핵심 원리 - 기본편     - Sec07 의존관계 자동 주입         - Pt05 조회 빈이 2개이상 - 문제

--- Spring/[kyh-p2] Spring Basic (OOP, Spring Contrainer, Spring Bean)/core/src/main/java/xyz/mon0mon/core/AppConfig.java
+++ Spring/[kyh-p2] Spring Basic (OOP, Spring Contrainer, Spring Bean)/core/src/main/java/xyz/mon0mon/core/AppConfig.java
@@ -48,9 +48,9 @@ public class AppConfig {
 
     @Bean
     public OrderService orderService() {
-//        System.out.println("call AppConfig.orderService");
-//        return new OrderServiceImpl(memberRepository(), discountPolicy());
-        return null;
+        System.out.println("call AppConfig.orderService");
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+//        return null;
     }
 
     @Bean
