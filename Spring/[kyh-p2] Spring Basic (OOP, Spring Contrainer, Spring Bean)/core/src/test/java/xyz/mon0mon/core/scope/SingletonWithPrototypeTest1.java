3c5f0be3f64bdb74ca886b406f84e6a774597a58
이민기 <mon0mon@naver.com>
2024-06-26 05:55:13 +0900

    📝 study - Track2 스프링 핵심 원리 - 기본편     - Sec09 빈 스코프         - Pt03 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제점

---
+++ Spring/[kyh-p2] Spring Basic (OOP, Spring Contrainer, Spring Bean)/core/src/test/java/xyz/mon0mon/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,84 @@
+package xyz.mon0mon.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonWithPrototypeTest1 {
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
+        assertThat(prototypeBean2.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        /**
+         * ClientBean은 싱글톤으로 생성될 때, 의존관게인 PrototypeBean도 같이 포함
+         * 따라서 ClientBean을 호출 할 때마다, 이미 생성된 PrototypeBean을 사용
+         * 결과적으로 Prototype Scope의 의미가 퇴색
+         */
+         ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(2);
+    }
+
+    @Scope("singleton")
+    static class ClientBean {
+//        @Autowired
+//        private ApplicationContext ac;
+        private final PrototypeBean prototypeBean;
+
+        ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init" + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
