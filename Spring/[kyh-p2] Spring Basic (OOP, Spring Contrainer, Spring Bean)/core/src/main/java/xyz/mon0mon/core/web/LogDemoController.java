7bbfeb391d052540a416a3fe77c28902277091d1
이민기 <mon0mon@naver.com>
2024-06-26 22:53:04 +0900

    📝 study - Track2 스프링 핵심 원리 - 기본편     - Sec09 빈 스코프         - Pt06 request 스코프 예제 만들기

---
+++ Spring/[kyh-p2] Spring Basic (OOP, Spring Contrainer, Spring Bean)/core/src/main/java/xyz/mon0mon/core/web/LogDemoController.java
@@ -0,0 +1,26 @@
+package xyz.mon0mon.core.web;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import xyz.mon0mon.core.common.MyLogger;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+  private final LogDemoService logDemoService;
+  private final MyLogger myLogger;
+
+  @RequestMapping("log-demo")
+  @ResponseBody
+  public String logDemo(HttpServletRequest request) {
+    String requestURL = request.getRequestURL().toString();
+    myLogger.setRequestUrl(requestURL);
+
+    myLogger.log("controller test");
+    logDemoService.logic("testId");
+    return "OK";
+  }
+}
---
+++ Spring/[kyh-p2] Spring Basic (OOP, Spring Contrainer, Spring Bean)/core/src/main/java/xyz/mon0mon/core/web/LogDemoService.java
@@ -0,0 +1,16 @@
+package xyz.mon0mon.core.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import xyz.mon0mon.core.common.MyLogger;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+  private final MyLogger myLogger;
+
+  public void logic(String id) {
+    myLogger.log("service id = " + id);
+  }
+}
