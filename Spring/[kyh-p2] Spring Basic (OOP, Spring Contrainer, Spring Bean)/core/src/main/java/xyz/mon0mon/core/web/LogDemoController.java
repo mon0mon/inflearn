77c0c09b496f1209961da235eabbee6c132b6c04
이민기 <mon0mon@naver.com>
2024-06-29 18:54:13 +0900

    📝 study - Track2 스프링 핵심 원리 - 기본편     - Sec09 빈 스코프         - Pt08 스코프와 프록시

--- Spring/[kyh-p2] Spring Basic (OOP, Spring Contrainer, Spring Bean)/core/src/main/java/xyz/mon0mon/core/web/LogDemoController.java
+++ Spring/[kyh-p2] Spring Basic (OOP, Spring Contrainer, Spring Bean)/core/src/main/java/xyz/mon0mon/core/web/LogDemoController.java
@@ -2,7 +2,6 @@ package xyz.mon0mon.core.web;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,13 +11,13 @@ import xyz.mon0mon.core.common.MyLogger;
 @RequiredArgsConstructor
 public class LogDemoController {
   private final LogDemoService logDemoService;
-  private final ObjectProvider<MyLogger> myLoggerProvider;
+  private final MyLogger myLogger;
 
   @RequestMapping("log-demo")
   @ResponseBody
   public String logDemo(HttpServletRequest request) throws InterruptedException {
     String requestURL = request.getRequestURL().toString();
-    MyLogger myLogger = myLoggerProvider.getObject();
+    System.out.println("myLogger = " + myLogger.getClass());
     myLogger.setRequestUrl(requestURL);
 
     myLogger.log("controller test");
