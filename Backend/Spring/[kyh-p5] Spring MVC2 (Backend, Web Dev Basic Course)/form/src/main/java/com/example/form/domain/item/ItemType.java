7d8ac1fff604dd099c69241b210da32670140bb7
이민기 <mon0mon@naver.com>
2024-08-04 16:01:38 +0900

    📝 study   - Track4 스프링 MVC 2편     - Sec02 타임리프 스프링 통합       - Pt04 요구사항 추가

---
+++ Backend/Spring/[kyh-p5] Spring MVC2 (Backend, Web Dev Basic Course)/form/src/main/java/com/example/form/domain/item/ItemType.java
@@ -0,0 +1,11 @@
+package com.example.form.domain.item;
+
+public enum ItemType {
+  BOOK("도서"), FOOD("음식"), ETC("기타");
+
+  private final String description;
+
+  ItemType(String description) {
+    this.description = description;
+  }
+}
