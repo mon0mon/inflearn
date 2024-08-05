d853692ab89b935c4a60911d5aec2737024a5313
이민기 <mon0mon@naver.com>
2024-08-05 21:52:40 +0900

    📝 study   - Track4 스프링 MVC 2편     - Sec03 메시지, 국제화       - Pt02 스프링 메시지 소스 설정

--- Backend/Spring/[kyh-p5] Spring MVC2 (Backend, Web Dev Basic Course)/message/src/main/java/hello/itemservice/MessageApplication.java
+++ Backend/Spring/[kyh-p5] Spring MVC2 (Backend, Web Dev Basic Course)/message/src/main/java/hello/itemservice/MessageApplication.java
@@ -1,7 +1,11 @@
 package hello.itemservice;
 
+import java.nio.charset.StandardCharsets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class MessageApplication {
@@ -9,5 +13,13 @@ public class MessageApplication {
   public static void main(String[] args) {
     SpringApplication.run(MessageApplication.class, args);
   }
+
+/*  @Bean
+  public MessageSource messageSource() {
+    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+    messageSource.setBasenames("messages", "errors");
+    messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
+    return messageSource;
+  }*/
 
 }
