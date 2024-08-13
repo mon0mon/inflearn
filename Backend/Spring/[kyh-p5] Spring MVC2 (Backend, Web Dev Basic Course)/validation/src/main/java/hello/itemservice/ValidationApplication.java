87bfc6d8a0fa1f2545ea888866fabea27b1e684b
이민기 <mon0mon@naver.com>
2024-08-13 22:11:13 +0900

    📝 study   - Track4 스프링 MVC 2편     - Sec04 검증1 - Validation       - Pt16 Validator 분리2

--- Backend/Spring/[kyh-p5] Spring MVC2 (Backend, Web Dev Basic Course)/validation/src/main/java/hello/itemservice/ValidationApplication.java
+++ Backend/Spring/[kyh-p5] Spring MVC2 (Backend, Web Dev Basic Course)/validation/src/main/java/hello/itemservice/ValidationApplication.java
@@ -1,13 +1,20 @@
 package hello.itemservice;
 
+import hello.itemservice.web.validation.ItemValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.validation.Validator;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ValidationApplication {
+public class ValidationApplication implements WebMvcConfigurer {
 
   public static void main(String[] args) {
     SpringApplication.run(ValidationApplication.class, args);
   }
 
+//  @Override
+//  public Validator getValidator() {
+//    return new ItemValidator();
+//  }
 }
