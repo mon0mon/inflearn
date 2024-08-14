51ddd6fd02b0a8e5522edba2b524ca18e5d20310
이민기 <mon0mon@naver.com>
2024-08-14 21:50:55 +0900

    📝 study   - Track4 스프링 MVC 2편     - Sec05 검증2 - Bean Validation       - Pt02 Bean Validation - 시작

--- Backend/Spring/[kyh-p5] Spring MVC2 (Backend, Web Dev Basic Course)/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ Backend/Spring/[kyh-p5] Spring MVC2 (Backend, Web Dev Basic Course)/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,22 @@
 package hello.itemservice.domain.item;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 public class Item {
 
     private Long id;
+    @NotBlank
     private String itemName;
+    @NotNull
+    @Range(min = 1_000, max = 1_000_000)
     private Integer price;
+    @NotNull
+    @Max(9_999)
     private Integer quantity;
 
     public Item() {
