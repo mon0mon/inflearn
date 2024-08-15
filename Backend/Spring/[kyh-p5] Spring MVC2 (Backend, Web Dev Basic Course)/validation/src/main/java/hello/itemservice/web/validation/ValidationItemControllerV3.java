85d58c6d06362d0cd8477ded4ee5b7ecbbbd05ca
이민기 <mon0mon@naver.com>
2024-08-15 09:40:15 +0900

    📝 study   - Track4 스프링 MVC 2편     - Sec05 검증2 - Bean Validation       - Pt06 Bean Validation - 오브젝트 오류

--- Backend/Spring/[kyh-p5] Spring MVC2 (Backend, Web Dev Basic Course)/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ Backend/Spring/[kyh-p5] Spring MVC2 (Backend, Web Dev Basic Course)/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -53,6 +53,15 @@ public class ValidationItemControllerV3 {
   public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult,
       RedirectAttributes redirectAttributes, Model model) {
 
+    //특정 필드 예외가 아닌 전체 예외
+    if (item.getPrice() != null && item.getQuantity() != null) {
+      int resultPrice = item.getPrice() * item.getQuantity();
+      if (resultPrice < 10_000) {
+        bindingResult.reject("totalPriceMin", new Object[]{10_000,
+            resultPrice}, null);
+      }
+    }
+
     // 검증에 실패하면 다시 입력 폼으로
     if (bindingResult.hasErrors()) {
       log.info("errors = {} ", bindingResult);
