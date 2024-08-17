5f30296c1082e927e8e987ae87e33616331c8308
이민기 <mon0mon@naver.com>
2024-08-17 23:10:24 +0900

    📝 study   - Track4 스프링 MVC 2편     - Sec06 로그인 처리1 - 쿠키, 세션       - Pt04 회원 가입

---
+++ Backend/Spring/[kyh-p5] Spring MVC2 (Backend, Web Dev Basic Course)/login/src/main/java/hello/login/domain/member/Member.java
@@ -0,0 +1,15 @@
+package hello.login.domain.member;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+public class Member {
+  private Long id;
+  @NotEmpty
+  private String loginId;   //  로그인 ID
+  @NotEmpty
+  private String name;      //  사용자 이름
+  @NotEmpty
+  private String password;  //  패스워드
+}
