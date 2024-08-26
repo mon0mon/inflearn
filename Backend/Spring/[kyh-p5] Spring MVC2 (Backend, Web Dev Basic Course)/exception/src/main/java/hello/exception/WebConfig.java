e156871f3a422852a34fe0162144880535b7590a
이민기 <mon0mon@naver.com>
2024-08-26 10:08:07 +0900

    📝 study   - Track4 스프링 MVC 2편     - Sec08 예외 처리와 오류 페이지       - Pt06 서블릿 예외 처리 - 인터셉터

--- Backend/Spring/[kyh-p5] Spring MVC2 (Backend, Web Dev Basic Course)/exception/src/main/java/hello/exception/WebConfig.java
+++ Backend/Spring/[kyh-p5] Spring MVC2 (Backend, Web Dev Basic Course)/exception/src/main/java/hello/exception/WebConfig.java
@@ -1,9 +1,11 @@
 package hello.exception;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
@@ -11,7 +13,16 @@ import jakarta.servlet.Filter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-  @Bean
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new LogInterceptor())
+        .order(1)
+        .addPathPatterns("/**")
+        .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");   // 오류 페이지 경로
+//        .excludePathPatterns("/css/**", "*.ico", "/error");   // 오류 페이지 경로
+  }
+
+//  @Bean
   public FilterRegistrationBean logFilter() {
     FilterRegistrationBean<Filter> filterRegistrationBean = new
         FilterRegistrationBean<>();
--- Backend/Spring/[kyh-p5] Spring MVC2 (Backend, Web Dev Basic Course)/exception/src/main/java/hello/exception/servlet/ErrorPageController.java
+++ Backend/Spring/[kyh-p5] Spring MVC2 (Backend, Web Dev Basic Course)/exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,8 +1,11 @@
 package hello.exception.servlet;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
